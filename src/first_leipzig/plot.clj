96c45bc0d236190b7c4940c3f2144a236288e174
Bobby Harris <bobby.harris@gmail.com>
2024-02-17 14:05:50 -0500

    starting ps shell work

--- src/first_leipzig/plot.clj
+++ src/first_leipzig/plot.clj
@@ -1,7 +1,10 @@
 (ns first-leipzig.plot
   {:clj-kondo/config '{:linters {:unresolved-symbol {:level :off}
                                  :invalid-arity {:level :off}}}}
-  (:require [oz.core :as oz]))
+  (:require [oz.core :as oz]
+            [clojure.java.shell :as shell]
+            [clojure.java.io :as io])
+  (:import   [java.io BufferedReader StringReader]))
 
 (defn plot [data]
   (let [formatted-data (for [y (map-indexed #(vector %1 %2) data)]
@@ -17,5 +20,8 @@
 (comment
   (plot [1 2 3 4 5]))
 
+(def ps (line-seq (BufferedReader.  (StringReader. (:out (shell/sh "ps" "-ef"))))))
 
+(count ps)
+(println (last ps))
 
